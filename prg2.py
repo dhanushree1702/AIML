ad85fc2e44c9a2eae262cc0bc4df8ab99f1f6648
zoro <zoro@gmail.com>
2023-08-16 14:30:25 +0530

    Change1

---
+++ prg2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr=np.array([[1,2,3],[1,2,3]])
+
+print("Dimension of the array = ",arr.ndim)
+print("Shape of the array = ",arr.shape)
+print("Datatype of the array is = ",arr.dtype)
+print("Size of the array is = ",arr.size)
